7715012b8a994a8488a9ab17997824c6215f87ee
Prathmesh <prathmesh4141@gmail.com>
2026-02-16 20:51:03 +0530

    fix render port configuration

--- server/index.js
+++ server/index.js
@@ -98,7 +98,12 @@ app.get("/champion", async (req, res) => {
 });
 
 
-app.listen(5000, () => console.log("Backend running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+});
+
 
 app.get("/user-history/:name", async (req, res) => {
   try {
